9c1e444522285a1a7181924213137d5b20817352
wesleyburlani <wesleyburlani@gmail.com>
2020-06-16 10:40:30 -0300

    [Refact] refatora arquivo main

--- src/main.cpp
+++ src/main.cpp
@@ -9,82 +9,102 @@
 #include <string>
 #include <vector>
 
-#define IndexOfId 0
+// Flag to turn on debug logs
 #define DEBUGGER 0
+// Column index which input file has id data
+#define IDINDEX 0
 // File used to mount B+ tree using bulk loading operation
 #define DATAFILEPATH "normalized-data.csv"
+// Alias to menu options
+#define OPT_PRINTTREE 1
+#define OPT_GETIDBYKEY 2
+#define OPT_REMOVENODE 3
+#define OPT_EXIT 9
 
 using namespace std;
 
+void printMenu();
+void optionPrintTree(Node *tree);
+void optionSearchPathByKey(Node *tree);
+void optionRemoveNode(Node *tree, CSVDatabase table, int treeOrder,
+                      int numberOfAttributes);
+
 int main() {
-
+  cout << "Welcome!\n";
   //-----------------Definitions-----------------
-  string _MESSAGE = "Welcome!\n";
-  cout << _MESSAGE;
-
   Node *tree = NULL;
   int numberOfAttributes;
   int numberOfCharsToIndex;
   int treeOrder;
-  string searched;
-  string toRemove;
   CSVDatabase table;
-  CSVDatabase _NoGroup;
-  stringstream stringStream;
-  char key[900];
 
   stdInDataCollect(numberOfAttributes, numberOfCharsToIndex, treeOrder);
 
-  bool success =
-      readCSV(DATAFILEPATH, table, IndexOfId, numberOfAttributes, numberOfCharsToIndex);
-  if (!success)
+  bool success = readCSV(DATAFILEPATH, table, IDINDEX, numberOfAttributes,
+                         numberOfCharsToIndex);
+  if (!success) {
     return 1;
+  }
 
   sort(table.begin(), table.end(), comparator(numberOfAttributes));
 
   bulkLoadingInsert(tree, table, treeOrder, numberOfAttributes);
 
-  if (DEBUGGER)
+  if (DEBUGGER) {
     printTree(tree, 0);
+  }
 
   cout << "\nOk.. The tree has been created. What you want to do ?\n\n";
 
-  bool exit = false;
-  while (!exit) {
+  int option;
+  printMenu();
+  while (scanf("%d", &option)) {
 
-    int option;
-    cout << "1.Print the tree\n";
-    cout << "2.Search a id by Key\n";
-    cout << "3.Remove a node\n";
-    cout << "9.Exit Program\n";
-    cout << "\nEnter with option: ";
-    scanf("%d", &option);
-
-    switch (option) {
-    case 1:
-      printTree(tree, 0);
-      cout << "Ok.. This is tree current tree.. and now?\n\n";
-      break;
-    case 2:
-      cout << "Alright!\nEnter the key value: ";
-      getline(cin, searched);
-      getline(cin, searched);
-      success = searchPathByKey(tree, searched, searched);
-      if (success)
-        cout << "The search returns this values:...\n" << searched << "\n\n";
-      else
-        cout << "Oops...Value not exists in tree\n\n";
-      break;
-    case 3:
-      cout << "Enter with the key id: ";
-      cin >> toRemove;
-      removeNode(toRemove, tree, table, treeOrder, numberOfAttributes);
-      break;
-    case 9:
-      exit = true;
-      break;
-    default:
+    if (option == OPT_PRINTTREE) {
+      optionPrintTree(tree);
+    } else if (option == OPT_GETIDBYKEY) {
+      optionSearchPathByKey(tree);
+    } else if (option == OPT_REMOVENODE) {
+      optionRemoveNode(tree, table, treeOrder, numberOfAttributes);
+    } else if (option == OPT_EXIT) {
+      return 0;
+    } else {
       cout << "Oops...this option not exists.\n\n";
     }
+    printMenu();
   }
+}
+
+void printMenu() {
+  cout << "1.Print the tree\n";
+  cout << "2.Search an id by Key\n";
+  cout << "3.Remove a node\n";
+  cout << "9.Exit Program\n";
+  cout << "\nEnter with option: ";
+}
+
+void optionPrintTree(Node *tree) {
+  printTree(tree, 0);
+  cout << "Ok.. This is the current tree.. and now?\n\n";
+}
+
+void optionSearchPathByKey(Node *tree) {
+  string input;
+  cout << "Alright!\nEnter the key value: ";
+  getline(cin, input);
+  getline(cin, input);
+  if (searchPathByKey(tree, input, input)) {
+    cout << "The search returns this values:...\n" << input << "\n\n";
+    return;
+  }
+
+  cout << "Oops...Value not exists in tree\n\n";
+}
+
+void optionRemoveNode(Node *tree, CSVDatabase table, int treeOrder,
+                      int numberOfAttributes) {
+  string input;
+  cout << "Enter with the key id: ";
+  cin >> input;
+  removeNode(input, tree, table, treeOrder, numberOfAttributes);
 }
