3a826668b18d7743c5c64be91d83fcf64037a514
wesleyburlani <wesleyburlani@gmail.com>
2020-06-16 11:17:58 -0300

    [Refact] refact csv file functions

--- csv/CSV.cpp
+++ csv/CSV.cpp
@@ -17,60 +17,56 @@ typedef CSVDatabase::const_iterator CSVDatabaseIterator;
 
 //--------------------------Headers-----------------------------
 
-bool readCSV(const char *FilePath, CSVDatabase &database, int _idColumn,
-             int _indexColumn, int n_CHAR);
-void readCSV(istream &input, CSVDatabase &database, int _idColumn,
-             int _indexColumn, int n_CHAR);
+bool readCSV(const char *filePath, CSVDatabase &database, int columnId,
+             int columnIndex, int numberOfCharsToIndex);
+void readCSV(istream &input, CSVDatabase &database, int columnId,
+             int columnIndex, int numberOfCharsToIndex);
 
 //--------------------------Functions-----------------------------
 
-bool readCSV(const char *FilePath, CSVDatabase &database, int _idColumn,
-             int _indexColumn, int n_CHAR) {
-
+bool readCSV(const char *filePath, CSVDatabase &database, int columnId,
+             int columnIndex, int numberOfCharsToIndex) {
   // Read the file in path 'FilePath'
-  fstream file(FilePath, std::ios::in);
+  fstream file(filePath, std::ios::in);
   // Verify if path is valid
   if (!file.is_open()) {
-    cout << "Function 'readCSV': \"" << FilePath << "\" not found!\n";
+    cout << "Function 'readCSV': \"" << filePath << "\" not found!\n";
     return false;
   }
   // Read file data
-  readCSV(file, database, _idColumn, _indexColumn, n_CHAR);
-
-  if (database.size() > 0)
+  readCSV(file, database, columnId, columnIndex, numberOfCharsToIndex);
+  if (database.size() > 0) {
     return true;
-
+  }
   cout << "Function 'readCSV': The table has no attributes.\n";
   return false;
 }
 
-void readCSV(istream &input, CSVDatabase &database, int _idColumn,
-             int _indexColumn, int n_CHAR) {
-
-  string csvLine;
+void readCSV(istream &input, CSVDatabase &database, int columnId,
+             int columnIndex, int numberOfCharsToIndex) {
+  string line;
   database.clear();
   int counter = 0;
 
   try {
     // Read every line from file
-    while (getline(input, csvLine)) {
+    while (getline(input, line)) {
       // Read current line from file
-      stringstream csvStream(csvLine); // Convert string to stringstream, cause
-                                       // is required to 'getLine'
-      CSVRow csvRow;
-      string csvCol;
+      // Convert string to stringstream, cause is required to 'getLine'
+      stringstream csvStream(line);
+      CSVRow row;
+      string column;
       // Read each column of current line in file
-      while (getline(csvStream, csvCol, ',')) {
-        csvCol = csvCol.substr(0, n_CHAR);
-        // csvCol.erase(remove(csvCol.begin(), csvCol.end(), '\"'),
-        // csvCol.end());
-        csvRow.push_back(csvCol);
+      while (getline(csvStream, column, ',')) {
+        column = column.substr(0, numberOfCharsToIndex);
+        row.push_back(column);
         counter++;
       }
       counter = 0;
       // Insert curren CSRow in database
-      if (csvRow.size() > 0)
-        database.push_back(csvRow);
+      if (row.size() > 0) {
+        database.push_back(row);
+      }
     }
   } catch (exception &e) {
     cout << "Function 'ReadCSV': Error reading this column in input.\n";
@@ -78,26 +74,27 @@ void readCSV(istream &input, CSVDatabase &database, int _idColumn,
 }
 
 void display(const CSVDatabase &database) {
-
   // Verifiy if file contains data
-  if (!database.size())
+  if (!database.size()) {
     return;
+  }
   // Get the first row
   CSVDatabaseIterator row = database.begin();
   // Foreach row
   int i = 0;
   for (; row != database.end(); ++row) {
     // Verify if row contains data
-    if (!row->size())
+    if (!row->size()) {
       continue;
+    }
     // Get the first column
     CSVRowIterator column = row->begin();
     // Print first column, to no show ','
-
     cout << i++ << "->" << *(column++);
     // Print others columns
-    for (; column != row->end(); ++column)
+    for (; column != row->end(); ++column) {
       cout << ',' << *column;
+    }
     // Broken line
     cout << "\n";
   }
